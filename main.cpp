11e5d9780be8442f05c73c9abfa7d17c3075ad7e
Ray Doetjes <rdoetjes@phonax.com>
2020-04-11 09:40:25 +0100

    motor duration implemented

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include "motor.h"
 #include "rangesensor.h"
 #include <iostream>
-#include <ncurses.h>
+#include <ncurses.h>			/* ncurses.h includes stdio.h */  
+#include <string.h> 
 
 #define D_STOP 0
 #define D_FORWARD 1
@@ -14,82 +15,84 @@
 #define D_HRIGHT 6
 #define CRUISE_SPEED 70
 
-void drive(Motor *m1, Motor *m2, int direction, int pwm1, int pwm2){
+unsigned int micros_next_move = 0;
+
+void drive(Motor *m1, Motor *m2, int direction, int pwm1, int pwm2, int msContinuousDrive){
   if (m1==NULL || m2==NULL)
     return;
 
+  //we instructed the motor to drive msContinuousDrive uninterrupted, we do handle a stop 
+  if ( micros() < micros_next_move && direction != D_STOP)
+    return;
+
+
   switch(direction) {
     case D_STOP:
-     m1->stop(); m2->stop(); break;
+     m1->stop(); m2->stop(); mvprintw(1,2, "MOVE: STOP"); break;
 
     case D_FORWARD:
-      m1->forward(pwm1); m2->forward(pwm2); break;
+      m1->forward(pwm1); m2->forward(pwm2); mvprintw(1,2, "MOVE: FORWARD"); break;
 
     case D_REVERSE:
-      m1->reverse(pwm1); m2->reverse(pwm2); break;
+      m1->reverse(pwm1); m2->reverse(pwm2); mvprintw(1,2, "MOVE: REVERSE"); break;
 
     case D_LEFT:
-      m1->stop(); m2->forward(pwm2); break;
+      m1->stop(); m2->forward(pwm2);        mvprintw(1,2, "MOVE: LEFT   "); break;
 
     case D_HLEFT:
-      m1->reverse(pwm1); m2->forward(pwm2); break;
+      m1->reverse(pwm1); m2->forward(pwm2); mvprintw(1,2, "MOVE: H_LEFT "); break;
 
     case D_RIGHT:
-      m1->forward(pwm1); m2->stop(); break;
+      m1->forward(pwm1); m2->stop();        mvprintw(1,2, "MOVE: RIGHT  "); break;
 
     case D_HRIGHT:
-      m1->forward(pwm1); m2->reverse(pwm2); break;
+      m1->forward(pwm1); m2->reverse(pwm2); mvprintw(1,2, "MOVE: H_RIGHT"); break;
   }
+
+  micros_next_move = micros() + (msContinuousDrive * 1000);
 }
 
-void demo(Motor *ml, Motor* mr){
-     delay(3000);
-     std::cout << "FW" << std::endl;
-     drive(ml, mr, D_FORWARD, 100, 100);
+void demo(Motor *ml, Motor* mr, int runSec){
+   mvprintw(0,0, "DEMO MOTOR: ");
 
-     delay(3000);
-     std::cout << "RE" << std::endl;
-     drive(ml, mr, D_REVERSE, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_FORWARD, 100, 100, 0);
 
-     delay(3000);
-     std::cout << "L" << std::endl;
-     drive(ml, mr, D_LEFT, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_REVERSE, 100, 100, 0);
 
-     delay(3000);
-     std::cout << "HL" << std::endl;
-     drive(ml, mr, D_HLEFT, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_LEFT, 100, 100, 0);
 
-     delay(3000);
-     std::cout << "R" << std::endl;
-     drive(ml, mr, D_RIGHT, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_HLEFT, 100, 100, 0);
 
-     delay(3000);
-     std::cout << "HR" << std::endl;
-     drive(ml, mr, D_HRIGHT, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_RIGHT, 100, 100, 0);
 
-     delay(3000);
-     drive(ml, mr, D_STOP, 100, 100);
+   delay(runSec * 1000);
+   drive(ml, mr, D_HRIGHT, 100, 100, 0);
+
+   delay(runSec * 1000);
+   drive(ml, mr, D_STOP, 100, 100, 0);
 }
 
 void move(Motor *ml, Motor *mr, RangeSensor *rs) {
   float range;
 
   range = rs->measure();
+  mvprintw(2,2, "RANGE: %.2f cm", range);
 
   if ( range > 30)
-     drive(ml, mr, D_FORWARD, CRUISE_SPEED, CRUISE_SPEED);
+     drive(ml, mr, D_FORWARD, CRUISE_SPEED, CRUISE_SPEED, 0);
   else if(range <30 && range >15) {
-     (rand()%2 == 0)?drive(ml, mr, D_HRIGHT, CRUISE_SPEED, CRUISE_SPEED):drive(ml, mr, D_HLEFT, CRUISE_SPEED, CRUISE_SPEED);
-     delay(600);
+     (rand()%2 == 0)?drive(ml, mr, D_HRIGHT, CRUISE_SPEED, CRUISE_SPEED, 800):drive(ml, mr, D_HLEFT, CRUISE_SPEED, CRUISE_SPEED, 800);
   }
   else{
-    drive(ml, mr, D_REVERSE, 100, 100);
-    delay(500);
-    (rand()%2 == 0)?drive(ml, mr, D_HRIGHT, 50, 50):drive(ml, mr, D_HLEFT, 50, 50);
-    delay(800);
+    drive(ml, mr, D_REVERSE, 100, 100, 500);
+    (rand()%2 == 0)?drive(ml, mr, D_HRIGHT, 50, 50, 800):drive(ml, mr, D_HLEFT, 50, 50, 800);
   }
 }
-
 
 int main(){
  
@@ -104,17 +107,26 @@ int main(){
      Motor *ml=new Motor(9, 8);
      Motor *mr=new Motor(16, 15);
      RangeSensor *rs = new RangeSensor(7, 0);
- 
+     int key;
+
+     mvprintw(0, 40, "ROBOT v0.1");
      while(1) {
        //stop robot when q is pressedÂ
-       if ( getch() == 'q') break;
+       key = getch();
+       if ( key == 'q' ) break;
+       if ( key == 'd' ) demo(ml, mr, 2);
+
        //move the robot
        move(ml, mr, rs);
+
+       delay(5);
      }
+
      //Restore stdin to its normal blocking operation before we leave.
      endwin();
-     drive(ml, mr, D_STOP, 0, 0);
+     drive(ml, mr, D_STOP, 0, 0, 0);
   
+     //destruct objects
      delete rs;
      delete mr;
      delete ml;
