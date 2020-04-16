0d76e2f12bcfa737006dee45ea23ac0cb2fad322
Ray Doetjes <rdoetjes@phonax.com>
2020-04-16 17:31:53 +0100

    stop to take learning pic

--- main.cpp
+++ main.cpp
@@ -18,9 +18,12 @@
 #define D_LEFT 5
 #define D_RIGHT 6
 #define CRUISE_SPEED 1000
+int oldDir = 0;
 
 using namespace cv;
 using namespace std;
+
+VideoCapture cap;
 
 //convenience structure, used by drive function
 struct driveMotors{
@@ -76,30 +79,54 @@ void drive(driveMotors *d){
   if ( micros() < d->micros_next_move && d->direction != D_STOP)
     return;
 
+  oldDir = d->direction;
+
   switch(d->direction) {
     case D_STOP:
-      d->mr->stop(); d->ml->stop(); 
+      d->mr->stop(); d->ml->stop(); break;
 
     case D_FORWARD:
-      d->mr->forward(d->pwm1); d->ml->forward(d->pwm2); 
+      d->mr->forward(d->pwm1); d->ml->forward(d->pwm2); break;
 
     case D_REVERSE:
-      d->mr->reverse(d->pwm1); d->ml->reverse(d->pwm2); 
+      d->mr->reverse(d->pwm1); d->ml->reverse(d->pwm2); break;
 
     case D_LEFT:
-      d->mr->stop(); d->ml->forward(d->pwm2);        
+      d->mr->stop(); d->ml->forward(d->pwm2); break;       
 
     case D_HLEFT:
-       d->mr->reverse(d->pwm1); d->ml->forward(d->pwm2); 
+       d->mr->reverse(d->pwm1); d->ml->forward(d->pwm2); break;
 
     case D_RIGHT:
-      d->mr->forward(d->pwm1); d->ml->stop();        
+      d->mr->forward(d->pwm1); d->ml->stop(); break;       
 
     case D_HRIGHT:
-      d->mr->forward(d->pwm1); d->ml->reverse(d->pwm2); 
+      d->mr->forward(d->pwm1); d->ml->reverse(d->pwm2); break;
   }
 
   d->micros_next_move = micros() + (d->msContinuous * 1000);
+}
+
+void takePic(const char *path, driveMotors *d){
+  int t = d->direction;
+
+  //stop current motion so we have motion blur free images
+  if (d->direction != oldDir){
+    d->direction = D_STOP;
+    drive(d);
+  } else { return; }
+
+  Mat frame, dst;
+  Size size(213,160);
+  delay(1600);
+  cap.read(frame);
+  resize(frame, dst ,size);
+
+  if( frame.empty() ) return; // end of video stream
+  flip(dst, frame, 0);
+  
+  cv::imwrite(path, frame);
+  imshow("pic", frame);
 }
 
 /*
@@ -130,15 +157,18 @@ void moveRobot(driveMotors *d, RangeSensor *rs, Mat mat) {
 
   //The random factors make this fuzzy (non intelligent but still less likely to get stuck
   if ( range > 30){
+     s  << "./1/" << file.str();
+     takePic(s.str().c_str(), d);
+
      d->direction = D_FORWARD;
      d->pwm1 = d->pwm2 = CRUISE_SPEED;
      d->msContinuous = 0;
      drive(d);
-     stringstream s;
-     s  << "./1/" << file.str();
-     cv::imwrite(s.str(), mat);
   }
-  else if(range <30 && range >15) {
+  else if(range <30 && range >10) {
+     s << "./2/" << file.str();
+     takePic(s.str().c_str(), d);
+
      if (rand()%2 == 0){ 
        d->pwm1 = d->pwm2 = 80;
        d->msContinuous = rand()%(1200-200 + 1) + 600;
@@ -151,16 +181,15 @@ void moveRobot(driveMotors *d, RangeSensor *rs, Mat mat) {
        d->direction = D_HRIGHT;
        drive(d);
      }
-     s << "./2/" << file.str();
-     cv::imwrite(s.str(), mat);
   }  
   else{
+    s << "./3/" << file.str();
+    takePic(s.str().c_str(), d);
+
     d->direction = D_REVERSE;
     d->pwm1 = d->pwm2 = CRUISE_SPEED;
     d->msContinuous = rand()%(1200-600 + 1) + 600;
     drive(d);
-    s << "./3/" << file.str();
-    cv::imwrite(s.str(), mat);
   }
 
   micros_next_measure = micros() + (100*1000);
@@ -181,7 +210,6 @@ void moveRobot(driveMotors *d, RangeSensor *rs, Mat mat) {
   putText(mat, srange.str().c_str(), Point2f(10,10), FONT_HERSHEY_PLAIN, 1,  Scalar(0,0,255,255));
   imshow("Camera", mat);
 }
-
 void createDir(){
   if ( dirExists("./1/") == 0 ) mkdir ("./1", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
   if ( dirExists("./2/") == 0 ) mkdir ("./2", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
@@ -191,14 +219,14 @@ void createDir(){
 int main(){
      createDir();
 
-     VideoCapture cap;
-
      int deviceID = 0;             // 0 = open default camera
      int apiID = cv::CAP_ANY;      // 0 = autodetect default API
      // open selected camera using selected API
+
      cap.open(deviceID + apiID);
      cap.set(CAP_PROP_FRAME_WIDTH, 640/2);
      cap.set(CAP_PROP_FRAME_HEIGHT, 480/2);
+
      // check if we succeeded
      if (!cap.isOpened()) {
         std::cerr << "ERROR! Unable to open camera\n";
